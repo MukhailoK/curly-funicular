05009f92b9bd929b4c01eeae51b09a36a46a3964
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2024-01-05 18:37:03 +0100

    fix relations bugs

--- backend/src/main/java/com/ait/grooming/repository/EmployeeRepository.java
+++ backend/src/main/java/com/ait/grooming/repository/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package com.ait.grooming.repository;
 
+import com.ait.grooming.model.Schedule;
 import com.ait.grooming.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface EmployeeRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
+
+    Optional<User> getAllScheduleByUserName(String username);
 }
