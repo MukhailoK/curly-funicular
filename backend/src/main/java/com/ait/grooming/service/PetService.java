6e45cb274b46f5f69f0aef304c5be9672195c826
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2024-01-18 13:09:31 +0100

    update petController createPet request
    add getAllBreeds(), getAllTypes, getPetByName()  methods

--- backend/src/main/java/com/ait/grooming/service/PetService.java
+++ backend/src/main/java/com/ait/grooming/service/PetService.java
@@ -1,15 +1,23 @@
 package com.ait.grooming.service;
 
-import com.ait.grooming.utils.request.PetRequestDto;
 import com.ait.grooming.model.Breed;
 import com.ait.grooming.model.Pet;
 import com.ait.grooming.model.User;
 import com.ait.grooming.repository.BreedRepository;
 import com.ait.grooming.repository.PetRepository;
 import com.ait.grooming.repository.UserRepository;
+import com.ait.grooming.service.exceptions.IsAlreadyExistException;
 import com.ait.grooming.service.exceptions.NotFoundException;
+import com.ait.grooming.utils.request.PetRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.ait.grooming.utils.maper.pet.PetMapper.toPetDto;
 
 @Service
 @RequiredArgsConstructor
@@ -19,33 +27,54 @@ public class PetService {
     private final BreedRepository breedRepository;
     private final UserRepository userRepository;
 
-    public boolean createPet(PetRequestDto requestDto) {
+    public ResponseEntity<?> createPet(PetRequest requestDto, Principal connectedUser) {
         if (!petRepository.existsByName(requestDto.getName())) {
-            try {
-                Breed breedById = breedRepository.findById(requestDto.getBreed().getId())
-                        .orElseThrow(() -> new NotFoundException("Breed with id " + requestDto.getBreed().getId() + " not found!"));
+            Breed breedById = breedRepository.findById(requestDto.getBreedId())
+                    .orElseThrow(() -> new NotFoundException("Breed with id " + requestDto.getBreedId() + " not found!"));
 
-                User userById = userRepository.findByEmail(requestDto.getOwnerEmail())
-                        .orElseThrow(() -> new NotFoundException("User with email " + requestDto.getOwnerEmail() + " not found!"));
+            User userById = userRepository.findByEmail(connectedUser.getName())
+                    .orElseThrow(() -> new NotFoundException("User with email " + connectedUser.getName() + " not found!"));
 
-
-
-                Pet pet = new Pet();
-                pet.setName(requestDto.getName());
-              //  pet.setOwner(requestDto.getOwner());
-                pet.setOwner(userById);
-                pet.setType(Pet.PetType.DOG);
-              //  pet.setBreed(requestDto.getBreed());
-                pet.setBreed(breedById);
-             //   pet.setPhotoUrl(requestDto.getPhotoUrl());
-                pet.setSpecial_notes(requestDto.getSpecialNotes());
-                petRepository.save(pet);
-                return true;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            Pet pet = new Pet();
+            pet.setName(requestDto.getName());
+            //  pet.setOwner(requestDto.getOwner());
+            pet.setOwner(userById);
+            pet.setType(Pet.PetType.valueOf(requestDto.getType().toUpperCase()));
+            //  pet.setBreed(requestDto.getBreed());
+            pet.setBreed(breedById);
+            //   pet.setPhotoUrl(requestDto.getPhotoUrl());
+            pet.setSpecial_notes(requestDto.getSpecialNotes());
+            petRepository.save(pet);
+            return ResponseEntity.ok(toPetDto(pet));
 
         }
-        return false;
+        throw new IsAlreadyExistException("Pet is already exist");
+    }
+
+    public ResponseEntity<?> getAllBreed() {
+        List<Breed> breeds = breedRepository.findAll();
+        if (breeds.isEmpty()) {
+            throw new NotFoundException("Breeds not found");
+        } else
+            return ResponseEntity.ok(breeds);
+
+    }
+
+    public ResponseEntity<?> findByPetName(String petName, Principal connectedUser) {
+        User owner = userRepository.findByEmail(connectedUser.getName())
+                .orElseThrow(() -> new NotFoundException("User not found"));
+        List<Pet> pets = petRepository
+                .findAllByOwner(owner)
+                .orElseThrow(() -> new NotFoundException("pets not found"));
+        Pet pet = pets.stream()
+                .filter(p -> p.getName().equals(petName))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException("Pet with name " + petName + " not found"));
+        return ResponseEntity.ok(toPetDto(pet));
+    }
+
+    public ResponseEntity<?> getAllTypes() {
+        List<String> petTypes = List.of(Arrays.toString(Pet.PetType.values()));
+        return ResponseEntity.ok(petTypes.stream().map(String::toLowerCase));
     }
 }
