e4919db3a429fd01a68363cd837b2b3346ec6eed
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2024-01-19 14:01:21 +0100

    add appointment contr to white list

--- backend/src/main/java/com/ait/grooming/config/SecurityConfiguration.java
+++ backend/src/main/java/com/ait/grooming/config/SecurityConfiguration.java
@@ -25,6 +25,7 @@ public class SecurityConfiguration {
             "/api/grooming/**",
             "/api/pets/breeds",
             "/api/pets/types",
+            "/api/appointment/**",
             "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
@@ -41,7 +42,6 @@ public class SecurityConfiguration {
             "/api/v1/users/**",
             "/api/employee/**",
             "/api-client/**",
-            "/api/appointment/**",
             "/api/pets/new",
             "/api/pets/findByName/**"
     };
