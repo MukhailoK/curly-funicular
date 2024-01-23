d811892f0240380d2fb546d052765583f0746c3b
OlenaDikarieva <olena.dikarieva@gmail.com>
2024-01-23 11:21:30 +0100

    Add AppointmentRequest, AppointmentResponseDto in appointments

---
+++ backend/src/main/java/com/ait/grooming/dto/appointment/AppointmentResponseDto.java
@@ -0,0 +1,21 @@
+package com.ait.grooming.dto.appointment;
+
+//import com.ait.grooming.dto.client.ClientDto;
+//import com.ait.grooming.dto.employee.EmployeeDto;
+import com.ait.grooming.dto.grooming.GroomingDto;
+import com.ait.grooming.dto.pet.PetDto;
+import com.ait.grooming.dto.user.UserDto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
+public class AppointmentResponseDto {
+    private UserDto userDto;
+    private GroomingDto groomingDto;
+    private PetDto petDto;
+    private LocalDateTime dateTimeStart;
+    private String status;
+}
