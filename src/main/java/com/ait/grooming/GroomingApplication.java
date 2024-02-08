c9ece57f2581b6570e13f8a4541994b5b67b2083
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2024-02-08 19:12:20 +0100

    finish clean up

--- src/main/java/com/ait/grooming/GroomingApplication.java
+++ src/main/java/com/ait/grooming/GroomingApplication.java
@@ -3,13 +3,17 @@ package com.ait.grooming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
-//@PropertySource("classpath:application-local.properties")
+@PropertySource("classpath:application-local.properties")
 public class GroomingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GroomingApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(GroomingApplication.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        environment.setActiveProfiles("develop");
     }
 }
